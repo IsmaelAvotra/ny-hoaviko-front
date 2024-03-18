dc825d32cb98d5a6700e80f2851b12e68e64c99b
AvotraIsmael <avotraismael4@gmail.com>
2024-03-18 00:15:32 +0000

    IAN: Project initiation, navbar & footer component

---
+++ src/app/layouts/Footer.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import FacebookIcon from '@mui/icons-material/Facebook'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import XIcon from '@mui/icons-material/X'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import Link from 'next/link'
+
+const Footer = () => {
+  return (
+    <div className='bg-[#02182B] px-2 py-4'>
+      <p className='text-center mb-4 text-lg'>Suivez nous sur les réseaux :</p>
+      <div className='socials flex text-[#FFFBBD] items-center justify-center gap-4 mb-4'>
+        <FacebookIcon />
+        <InstagramIcon />
+        <XIcon />
+        <LinkedInIcon />
+      </div>
+      <div className='flex items-center justify-center mb-2'>
+        <p>
+          Made with{' '}
+          <span className='mx-1 text-[#EE353E]'>
+            <FavoriteIcon />
+          </span>
+          <span className='mx-2'>by</span>
+        </p>
+        <Link href={'/'}>Ismael Avotra</Link>
+      </div>
+      <p className='text-center text-xs text-[#fefefe]/75'>
+        © Copyright 2024 | All Rights Reserved!
+      </p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/app/layouts/Navbar.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu'
+
+const Navbar = () => {
+  return (
+    <div className='bg-[#02182B] px-4 sm:px-6 py-2 flex items-center justify-between'>
+      <div className='logo w-[48px] sm:w-[56px] h-auto'>
+        <Image
+          src={'/logo-fictif.png'}
+          alt={'logo for ny hoaviko application'}
+          width={1000}
+          height={1000}
+        />
+      </div>
+      <div className='signbtn-menu flex items-center gap-4 sm:gap-6'>
+        <Link
+          href={'/'}
+          className=' border border-[#fefefe] text-[#fefefe] text-sm px-4 py-2 rounded'
+        >
+          Se connecter
+        </Link>
+        <MenuIcon className='text-[28px] text-[#fefefe]' />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
